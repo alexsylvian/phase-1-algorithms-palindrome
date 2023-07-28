1c683576f6712cb4d39e233287b4cc5770499767
alexsylvian <lazeratus1@gmail.com>
2023-07-28 15:43:58 -0400

    made palindrome function and wrote pseudocode

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let oneString = word
+  let splitString = oneString.split('')
+  let reversedString = splitString.reverse()
+  let joinedString = reversedString.join('')
+  if (word === joinedString){
+    return true
+  }else{
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+  apply parameter string to a variable
+  split variable string into an array of strings for each letter
+  reverse the array
+  join the reversed array back into a single string
+  compare the original parameter string to reversed string
+    if match, return true
+    if not a match, return false
 */
 
 /*
@@ -20,6 +34,32 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  oneString = "racecar"
+  console.log(oneString)
+  let splitString = oneString.split('')
+  console.log(splitString)
+  let reversedString = splitString.reverse()
+  console.log(reversedString)
+  let joinedString = reversedString.join('')
+  console.log(joinedString)
+  console.log(isPalindrome(oneString))
 }
 
 module.exports = isPalindrome;
+
+
+
+
+
+
+
+
+// function isPalindrome(str) {
+//   // Remove non-alphanumeric characters and convert to lowercase
+//   const cleanedString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+//   // Compare the cleaned string with its reverse
+//   return cleanedString === cleanedString.split('').reverse().join('');
+// }
+
